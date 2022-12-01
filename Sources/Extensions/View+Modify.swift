079b744aac6aa05d1d5f3697fd74c2203862e79d
Fernando Fernandes <mfernandes.fernando@gmail.com>
2022-12-01 21:33:36 +0100

    Add View+Modify

---
+++ Sources/Extensions/View+Modify.swift
@@ -0,0 +1,41 @@
+import SwiftUI
+
+public extension View {
+    
+    /// Allows for using view modifiers between different iOS versions.
+    ///
+    /// Usage example:
+    ///
+    /// ```
+    /// MyView()
+    ///   .modify {
+    ///     if #available(iOS 16.0, *) {
+    ///       $0.sheet(isPresented: $isPresented) {
+    ///         MyModal()
+    ///           .presentationDetents([.height(200)]) // Only in iOS 16
+    ///         }
+    ///     } else {
+    ///       $0.sheet(isPresented: $isPresented) {
+    ///         MyModal()
+    ///     }
+    /// ```
+    /// or without fallback:
+    ///
+    /// ```
+    /// Text("LOLSTRING")
+    ///   .modify {
+    ///     if #available(iOS 15.0, *) {
+    ///       $0.badge(2)
+    ///     }
+    ///     // Fallback not required. The view will stay untouched.
+    /// ```
+    ///
+    /// [source](https://stackoverflow.com/a/71203870/584548)
+    @ViewBuilder func modify<Content: View>(@ViewBuilder _ transform: (Self) -> Content?) -> some View {
+        if let view = transform(self), !(view is EmptyView) {
+            view
+        } else {
+            self
+        }
+    }
+}
