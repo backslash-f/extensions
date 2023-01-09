d1aab5a460034d4ad42485f994bb82c6a91b2060
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-01-09 09:48:02 +0100

    Add CGFloat + Degrees

---
+++ Sources/Extensions/Foundation/CGFloat+Degrees.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public extension CGFloat {
+    static let degrees_90    = CGFloat.pi / 2
+    static let degrees_180   = CGFloat.pi
+    static let degrees_270   = CGFloat.pi + (CGFloat.pi / 2)
+    static let degrees_360   = 2 * CGFloat.pi
+}
