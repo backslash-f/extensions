72db754c7904c9993040a8f1fe6b90b5fb5f5ef6
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-02-22 15:18:41 +0100

    Add the FadeOut view modifier

---
+++ Sources/Extensions/SwiftUI/FadeOut.swift
@@ -0,0 +1,51 @@
+import SwiftUI
+
+/// Fades out the edge of a view, based on a given `FadeOut.Edge`.
+public struct FadeOut: ViewModifier {
+    /// The edge to be faded out.
+    public let edge: Edge
+
+    public enum Edge: String {
+        case top
+        case bottom
+        case leading
+        case trailing
+    }
+
+    public func body(content: Content) -> some View {
+        content
+            .mask(
+                LinearGradient(
+                    gradient: Gradient(
+                        stops: [
+                            .init(color: .black, location: 0),
+                            .init(color: .clear, location: 1)
+                        ]
+                    ),
+                    startPoint: params.startPoint,
+                    endPoint: params.endPoint
+                )
+            )
+    }
+
+    // MARK: - Private
+
+    private var params: (startPoint: UnitPoint, endPoint: UnitPoint) {
+        switch edge {
+        case .top:
+            return (.bottom, .top)
+        case .bottom:
+            return (.top, .bottom)
+        case .leading:
+            return (.trailing, .leading)
+        case .trailing:
+            return (.leading, .trailing)
+        }
+    }
+}
+
+public extension View {
+    func fadeOut(edge: FadeOut.Edge) -> some View {
+        modifier(FadeOut(edge: edge))
+    }
+}
